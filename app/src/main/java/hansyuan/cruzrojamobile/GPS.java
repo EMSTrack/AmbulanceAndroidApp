219313af325dc91a5295794b005859afaa39f5d8
hansyuan <hanzzeh@gmail.com>
2016-10-29 16:45:37 -0700

    Added update button to update current lon/lat. Also dropdownlist of status updates

--- app/src/main/java/hansyuan/cruzrojamobile/GPS.java
+++ app/src/main/java/hansyuan/cruzrojamobile/GPS.java
@@ -1,11 +1,16 @@
 package hansyuan.cruzrojamobile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,7 +31,18 @@ public class GPS extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
 
-        doStuff();
+        //doStuff();
+        doStuffWrapper();
+
+        // Dropdown Menu (spinner)
+        Spinner spinner = (Spinner) findViewById(R.id.statusupdate);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.status_updates, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     /**
@@ -81,8 +97,22 @@ public class GPS extends AppCompatActivity {
        // this.display(s);
     }
 
+    private void doStuffWrapper() {
+            // Execute some code after 2 seconds have passed
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doStuff();
+                }
+            }, 2000);
+    }
 
 
+    public void UpdateGPS(View view) {
+
+        doStuff();
+    }
 
 
 }
