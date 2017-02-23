71d2fbf2dc1608a9897b230759b8bc18ab71d6b9
hans <h3yuan@eng.ucsd.edu>
2017-02-22 17:55:16 -0800

    nonimportant deletion of commented code

--- app/src/main/java/hansyuan/cruzrojamobile/GPS.java
+++ app/src/main/java/hansyuan/cruzrojamobile/GPS.java
@@ -230,46 +230,8 @@ public class GPS extends AppCompatActivity  implements CompoundButton.OnCheckedC
     }
 
 
-    /**
-     *
-     * @param view
-     */
-    public void UpdateGPS(View view) {
-        tryGPS();
-    }
 
-    /**
-     *
-     * @param view
-     */
     public void broadcast(View view){
-        /*
-        final TextView mTextView = (TextView) findViewById(R.id.text);
-
-
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                      //  mTextView.setText("Response is: "+ response.substring(0,500));
-                        toasting("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                //mTextView.setText("That didn't work!");
-                toasting("That didn't work!!!!");
-            }
-        });
-    // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-        */
         broadcast();
     }
 
@@ -301,7 +263,7 @@ public class GPS extends AppCompatActivity  implements CompoundButton.OnCheckedC
                 toasting("That didn't work!!!!");
             }
         });
-// Add the request to the RequestQueue.
+        // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
 
