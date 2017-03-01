38d57c0bc731ca5154d803c6a7653ef121e7cad8
jkapich <jkapich@gmail.com>
2017-02-28 16:33:34 -0800

    trying to pull

--- app/src/main/java/hansyuan/cruzrojamobile/GPSTracker.java
+++ app/src/main/java/hansyuan/cruzrojamobile/GPSTracker.java
@@ -71,11 +71,6 @@ public class GPSTracker extends Service implements LocationListener {
 
 
 
-
-    
-
-
-
     public GPSTracker(Context context) {
         this.mContext = context;
         getLocation();
